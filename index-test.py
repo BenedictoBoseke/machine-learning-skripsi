fd9993012f8bd5e9ef942be0b1098146cb388df8
Benedicto Boseke <benedictoboseke@gmail.com>
2023-11-15 06:59:49 +0700

    switch test to main file

--- index-test.py
+++ index-test.py
@@ -1,58 +1,39 @@
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier  # Import the K-Nearest Neighbors classifier
+import pandas as pd 
+import numpy as np 
+import sklearn
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split
 
 malData = pd.read_csv("C:\\Users\\USER\\Desktop\\machine-learning-skripsi\\malware_dataset.csv", sep="|")
 
-legit = malData.iloc[0:41323].drop(["legitimate"], axis=1)
-mal = malData.iloc[41323:].drop(["legitimate"], axis=1)
+legit = malData[0:41323].drop(["legitimate"], axis=1)
+mal = malData[41323::].drop(["legitimate"], axis=1)
 
 data_in = malData.drop(['Name', 'md5', 'legitimate'], axis=1).values
-labels = malData['legitimate'].values
+labels = malData['legitimate'].values 
 
 extraTrees = ExtraTreesClassifier().fit(data_in, labels)
-
+  
 select = SelectFromModel(extraTrees, prefit=True)
 data_in_new = select.transform(data_in)
 
+features = data_in_new.shape[1]
+importances = extraTrees.feature_importances_
+
+indices = np.argsort(importances)[::-1]
+
 legit_train, legit_test, mal_train, mal_test = train_test_split(data_in_new, labels, test_size=0.2)
 
-# Random Forest Classifier
-classif_rf = RandomForestClassifier(n_estimators=50)
-classif_rf.fit(legit_train, mal_train)
+classif = RandomForestClassifier(n_estimators=50)
+classif.fit(legit_train, mal_train)
 
-results_rf = classif_rf.predict(legit_test)
-conf_mat_rf = confusion_matrix(mal_test, results_rf)
+results = classif.predict(legit_test)
+conf_mat = confusion_matrix(mal_test, results)
 
-print("Random Forest Classifier:")
-print("False positives: ", conf_mat_rf[0][1] / sum(conf_mat_rf)[0] * 100)
-print("False negatives: ", conf_mat_rf[1][0] / sum(conf_mat_rf)[1] * 100)
-print("Algorithm score: ", classif_rf.score(legit_test, mal_test) * 100)
+print("False positives: ", conf_mat[0][1] / sum(conf_mat)[0] * 100)
+print("False negatives: ", conf_mat[1][0] / sum(conf_mat)[1] * 100)
 
-# Gradient Boosting Classifier
-classif_gb = GradientBoostingClassifier(n_estimators=50)
-classif_gb.fit(legit_train, mal_train)
-
-results_gb = classif_gb.predict(legit_test)
-conf_mat_gb = confusion_matrix(mal_test, results_gb)
-
-print("\nGradient Boosting Classifier:")
-print("False positives: ", conf_mat_gb[0][1] / sum(conf_mat_gb)[0] * 100)
-print("False negatives: ", conf_mat_gb[1][0] / sum(conf_mat_gb)[1] * 100)
-print("Algorithm score: ", classif_gb.score(legit_test, mal_test) * 100)
-
-# K-Nearest Neighbors (KNN) Classifier
-classif_knn = KNeighborsClassifier(n_neighbors=5)  # You can specify the number of neighbors
-classif_knn.fit(legit_train, mal_train)
-
-results_knn = classif_knn.predict(legit_test)
-conf_mat_knn = confusion_matrix(mal_test, results_knn)
-
-print("\nK-Nearest Neighbors (KNN) Classifier:")
-print("False positives: ", conf_mat_knn[0][1] / sum(conf_mat_knn)[0] * 100)
-print("False negatives: ", conf_mat_knn[1][0] / sum(conf_mat_knn)[1] * 100)
-print("Algorithm score: ", classif_knn.score(legit_test, mal_test) * 100)
+print("Algorithm score: ", classif.score(legit_test, mal_test) * 100)
